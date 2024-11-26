f4a6e8a4e369730e168e446e748e06efa7caf7c3
Beloto21 <nicolasbelotoarmenio9@gmail.com>
2024-11-26 19:57:41 -0300

    Atividade Pronta

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -4,9 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "produtos") 
 public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,7 +16,6 @@ public class Produto {
     private String nome;
     private Double preco;
 
-    // Getters e Setters
     public Long getId() {
         return id;
     }
